a1400b2e0df00bdd982e4b62dd8da7306846f84e
Aditi Atul Vaidya <aditivaidya10@gmail.com>
2025-02-13 20:26:50 +0530

    Delete SQL/Check Data Overview.sql

--- SQL/Check Data Overview.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM sales_data LIMIT 10; -- This gives a preview of the dataset.
-
--- Helps identify column names and data types.
-SELECT COLUMN_NAME, DATA_TYPE 
-FROM INFORMATION_SCHEMA.COLUMNS 
-WHERE TABLE_NAME = 'sales_data';
